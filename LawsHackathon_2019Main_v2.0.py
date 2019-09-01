15c791407147f7fce3b055ead6c321dc03238d6d
TinoHsu <mssuperss@gmail.com>
2019-09-02 00:22:28 +0800

    更新少量停用字,實現二階(2rd)聚類功能

--- LawsHackathon_2019Main_v2.0.py
+++ LawsHackathon_2019Main_v2.0.py
@@ -3,6 +3,8 @@ from LawsHackathon_2019Tools import *
 ## 基礎設定區-----------------------
 ## 設定案由
 case = "侵權行為損害賠償"
+# case = "確認所有權存在"
+
 
 ## 核心詞與法條加權的係數
 coreWeighting = 1
@@ -54,10 +56,30 @@ docVector, words_list = bow1.wordsTokenization(seg_corpus, core_factor=coreWeigh
 
 # seg_corpus = segmentation(work_path, raw_text)
 # docVector, words_list = wordsTokenization(work_path, seg_corpus, core_factor=coreWeighting, tfidf_func = tfidf)
-
-
+print('\n')
+print('執行1st聚類')
 ## 依照文件矩陣中詞頻進行主題分類
 model_result = topicModelLDA(docVector, words_list, topic_k=5, n_top_words=show_top_words)
+
+## 執行2rd聚類
+for i in range(np.unique(model_result).shape[0]):
+    ## 取出目標Topic=?的文章在list中的index
+    result_index = np.squeeze(np.argwhere(model_result==i).T)
+    print('\n')
+    print('Topic', i, '執行2rd聚類')
+    print('目標文本的索引', result_index)
+    result_index_list = result_index.tolist()
+
+    ## 依照index重新合成2rd文本
+    seg_corpus_for_2rd = []
+    for index in result_index_list:
+        # print(index)
+        seg_corpus_for_2rd.append(seg_corpus[index])
+
+    ## 2rd聚類的過程
+    print('2rd聚類結果')
+    docVector_2rd, words_list_2rd = bow1.wordsTokenization(seg_corpus_for_2rd, core_factor=coreWeighting, tfidf_func = tfidf)
+    model_result_2rd = topicModelLDA(docVector_2rd, words_list_2rd, topic_k=3, n_top_words=show_top_words)
 
 
 # 隨機印出?篇?Topic檢查內容
